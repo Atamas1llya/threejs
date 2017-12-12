257c7eda173070ffc36f2a8f584ff16df260701a
Ilya Atamas <atamas1llya@gmail.com>
2017-12-12 18:17:14 +0200

    Simple physics example

--- app/js/Renderer.js
+++ app/js/Renderer.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import CANNON from 'cannon';
 
 import Block from './entities/Block';
 
@@ -17,6 +18,37 @@ export default class Renderer {
     this.scene.fog = new THREE.Fog(0x222233, 0, 20000);
     this.renderer.setClearColor('#212121', 1);
 
+    const world = new CANNON.World();
+    world.gravity.set(0,-2, 0);
+    world.broadphase = new CANNON.NaiveBroadphase();
+    world.solver.iterations = 10;
+
+    this.timeStep = 1 / 60;
+    this.world = world;
+
+    const boxShape = new CANNON.Box(new CANNON.Vec3(1,1,1));
+    const boxBody = new CANNON.Body({ mass: 1 });
+    boxBody.position.set(5, 10, 5);
+    boxBody.addShape(boxShape);
+    this.box = boxBody;
+    this.world.add(boxBody);
+
+    const boxShape2 = new CANNON.Box(new CANNON.Vec3(1,1,1));
+    const boxBody2 = new CANNON.Body({ mass: 0 });
+    boxBody2.position.set(5, 5, 5);
+    boxBody2.addShape(boxShape2);
+    this.box2 = boxBody2;
+    this.world.add(boxBody2);
+
+    const block = new Block({
+      size: [1, 1, 1],
+      position: [3, 3, 3],
+      color: 'green',
+    })
+    this.renderElement(block.mesh);
+    this.block = block.mesh
+
+
     window.addEventListener('resize', this._resize);
   }
 
@@ -30,7 +62,10 @@ export default class Renderer {
   }
 
 
-  renderElement = element => this.scene.add(element);
+  renderElement = element => {
+    const { x, y, z } = element.scale;
+    this.scene.add(element);
+  };
 
 
   renderMap = (map) => {
@@ -48,9 +83,17 @@ export default class Renderer {
     }
   }
 
+  _updatePhysics = () => {
+      this.world.step(1/60);
+      // Copy coordinates from Cannon.js to Three.js
+      this.block.position.copy(this.box.position);
+      this.block.quaternion.copy(this.box.quaternion);
+  }
+
 
   _animate = () => {
     requestAnimationFrame(this._animate);
+    this._updatePhysics();
 
     this.user.animateMovementTick();
 
